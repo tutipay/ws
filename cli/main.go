39c49ad36c235a329b6cbbe0c5dddfcf4771a054
Mohamed Yousif <mmbusif@gmail.com>
2022-09-09 18:56:40 +0200

    Add cli with websockets browser clients cli now has two clients (69) and other clients

--- cli/main.go
+++ cli/main.go
@@ -1,15 +1,34 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
-	"github.com/tutipay/chat"
+	chat "github.com/tutipay/ws"
 )
 
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	log.Println(r.URL)
+	if r.URL.Path == "/69" {
+		http.ServeFile(w, r, "69.html")
+		return
+	}
+	if r.URL.Path != "/" {
+		http.Error(w, "Not found", http.StatusNotFound)
+		return
+	}
+
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	http.ServeFile(w, r, "home.html")
+}
 func main() {
 	hub := chat.NewHub()
-	go hub.run()
+	go hub.Run()
 	mux := http.NewServeMux()
+	mux.HandleFunc("/", serveHome)
 	mux.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
 		chat.ServeWs(hub, w, r)
 	})

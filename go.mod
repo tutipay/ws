9844c414e03e939f6320b8b87094e1369b6db2a8
Mohamed Yousif <mmbusif@gmail.com>
2022-09-13 14:00:02 +0200

    add redis subscriber code

--- go.mod
+++ go.mod
@@ -2,4 +2,12 @@ module github.com/tutipay/ws
 
 go 1.18
 
-require github.com/gorilla/websocket v1.5.0
+require (
+	github.com/go-redis/redis/v9 v9.0.0-beta.2
+	github.com/gorilla/websocket v1.5.0
+)
+
+require (
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+)
---
+++ pub.go
@@ -0,0 +1,50 @@
+package chat
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/go-redis/redis/v9"
+)
+
+//CashoutPub subscribes to messages occurs in chan_cashouts. We should push onto this channel via a publisher
+func CashoutPub(r *redis.Client) {
+	ctx := context.Background()
+	pubsub := r.Subscribe(ctx, "chan_cashouts")
+
+	// Wait for confirmation that subscription is created before publishing anything.
+	_, err := pubsub.Receive(ctx)
+	if err != nil {
+		log.Printf("There is an error in connecting to chan.")
+		return
+	}
+
+	// // Go channel which receives messages.
+	ch := pubsub.Channel()
+
+	// Consume messages.
+	var message Message
+	for msg := range ch {
+		// So this is how we gonna do it! So great!
+		// we have to parse the payload here:
+		if err := json.Unmarshal([]byte(msg.Payload), &message); err != nil {
+			log.Printf("Error in marshaling data: %v", err)
+			continue
+		}
+
+		data, err := json.Marshal(&message)
+		if err != nil {
+			log.Printf("Error in marshaling response: %v", err)
+			continue
+		}
+		_, err = http.Post(message.Endpoint, "application/json", bytes.NewBuffer(data))
+		if err != nil {
+			log.Printf("Error in response: %v", err)
+		}
+		fmt.Println(msg.Channel, msg.Payload)
+	}
+}
